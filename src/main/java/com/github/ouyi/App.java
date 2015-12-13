a12b2629e3084d5eb7ae9724ae69dd56db429091
Yi Ou <yi.ou@adtech.com>
2015-12-13 23:18:37 +0100

    Add MultipleOutputsMapper

--- src/main/java/com/github/ouyi/App.java
+++ src/main/java/com/github/ouyi/App.java
@@ -2,18 +2,45 @@ package com.github.ouyi;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
  * Hello world!
  */
 public class App extends Configured implements Tool {
+
+  static class MultipleOutputsMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+
+    private MultipleOutputs<NullWritable, Text> mos;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+      this.mos = new MultipleOutputs<>(context);
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+      mos.write(NullWritable.get(), value, value.toString().split("\t")[0]);
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+      mos.close();
+    }
+  }
 
   public static void main(String[] args) throws Exception {
     System.out.println("Called main with args: " + Arrays.deepToString(args));
@@ -27,6 +54,7 @@ public class App extends Configured implements Tool {
     job.setJarByClass(getClass());
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(MultipleOutputsMapper.class);
     job.setNumReduceTasks(0);
     return job.waitForCompletion(true) ? 0 : 1;
   }
