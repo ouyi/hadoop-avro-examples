cc2e794e0f8ca0737a6ca9d6597d375c5b1edfec
Yi Ou <yi.ou@adtech.com>
2016-01-16 21:46:43 +0100

    Compiles but fails with GenericData cannot be cast to org.github.ouyi.avro.Imps

---
+++ src/main/java/com/github/ouyi/AvroMapOnly.java
@@ -0,0 +1,66 @@
+package com.github.ouyi;
+
+import com.github.ouyi.avro.Imps;
+import org.apache.avro.Schema;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+public class AvroMapOnly extends Configured implements Tool {
+
+    static class AvroIdentityMapper extends Mapper<AvroKey<Imps>, NullWritable, AvroKey<Imps>, NullWritable> {
+
+        @Override
+        protected void map(AvroKey<Imps> key, NullWritable value, Context context)
+                throws IOException, InterruptedException {
+            Imps record = key.datum();
+            int id = record.getId();
+            if (id != 3) {
+                Imps r = Imps.newBuilder().setId(record.getId()).setName(record.getName()).setCount(record.getCount()).build();
+                context.write(new AvroKey(r), NullWritable.get());
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new AvroMapOnly(), args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        System.out.println("Running with args: " + Arrays.deepToString(args));
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(getClass());
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        Schema schema = new Schema.Parser().parse(new File(args[2]));
+        job.setInputFormatClass(AvroKeyInputFormat.class);
+        AvroJob.setInputKeySchema(job, schema);
+        job.setOutputFormatClass(AvroKeyOutputFormat.class);
+        AvroJob.setOutputKeySchema(job, schema);
+        job.setMapperClass(AvroIdentityMapper.class);
+
+        job.setNumReduceTasks(0);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+}
