dab316a37948ea7d2917eb592e0f69f25d4398f5
Yi Ou <yi.ou@adtech.com>
2016-02-08 22:42:33 +0100

    Add AvroWriteGenericRecordTest

--- src/test/java/com/github/ouyi/AvroWriteGenericRecordTest.java
+++ src/test/java/com/github/ouyi/AvroWriteGenericRecordTest.java
@@ -5,9 +5,13 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,14 +20,14 @@ import java.net.URL;
 /**
  * Unit test for retrieving nested record.
  */
-public class AvroReadGenericRecordTest extends TestCase {
+public class AvroWriteGenericRecordTest extends TestCase {
 
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AvroReadGenericRecordTest(String testName) {
+    public AvroWriteGenericRecordTest(String testName) {
         super(testName);
     }
 
@@ -31,10 +35,10 @@ public class AvroReadGenericRecordTest extends TestCase {
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(AvroReadGenericRecordTest.class);
+        return new TestSuite(AvroWriteGenericRecordTest.class);
     }
 
-    public void testRead() throws IOException {
+    public void testWrite() throws IOException {
 
         URL url = this.getClass().getClassLoader().getResource("input/Company.avsc");
         assertNotNull(url);
@@ -44,19 +48,33 @@ public class AvroReadGenericRecordTest extends TestCase {
         DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
         // Another way of loading a file
         File file = new File("src/test/resources/input/companies.avro");
-        System.out.println(file.getAbsolutePath());
         DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(file, datumReader);
+
+        File fileOut = new File("target/companies2.avro");
+        Schema schemaOut = new Schema.Parser().parse(new File("src/test/resources/input/Company2.avsc"));
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schemaOut);
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter);
 
         GenericRecord company = null;
         int count = 0;
         while (dataFileReader.hasNext()) {
             company = dataFileReader.next(company);
-            GenericRecord address = (GenericRecord) company.get("address");
             if (company.get("name").toString().equals("aol")) {
+                dataFileWriter.create(schemaOut, fileOut);
+
+                GenericRecord recordOut = new GenericData.Record(schemaOut);
+                recordOut.put("id", company.get("id"));
+                recordOut.put("name", company.get("name"));
+                GenericRecord address = new GenericData.Record((GenericData.Record) company.get("address"), true);
+                recordOut.put("address", address);
+
+                dataFileWriter.append(recordOut);
+
                 count ++;
-                assertEquals(address.get("city").toString(), "NY City");
             }
         }
         assertTrue(count > 0);
+
+        dataFileWriter.close();
     }
 }
